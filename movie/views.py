27502b3d14c07241ab2610df6227ee1cb5d63130
Tazana2 <danisanmez@gmail.com>
2024-07-31 11:02:33 -0500

    movies search

--- movie/views.py
+++ movie/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .models import Movie
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {"name": "Daniel Santana Meza"})
+    # return render(request, 'home.html', {"name": "Daniel Santana Meza"})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {"movies": movies, "searchTerm": searchTerm})
 
 def about(request):
     return render(request, 'about.html')
